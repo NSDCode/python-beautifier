167f053373298a5dd0780d01e91dde67563d530c
NSDCode <47573987+NSDCode@users.noreply.github.com>
2020-05-18 08:19:15 +0200

    First release
    simple python formatting and cleaning

---
+++ PythonUnpacker/PythonUnpacker/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace PythonUnpacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Python Beautifier by Prometheo";
+            Console.ForegroundColor = ConsoleColor.Blue;
+            animation();
+            Console.WriteLine("Python Beautifier /o/");
+              string clear = Unpacker.Unpack(args[0]);
+              File.WriteAllText($@"{Environment.CurrentDirectory}\Cleared.py", clear);
+
+
+
+
+
+
+            void animation()
+            {
+                Console.WriteLine("[->]");
+                Thread.Sleep(100);
+                Console.Clear();
+                Console.WriteLine("[-->]");
+                Thread.Sleep(100);
+                Console.Clear();
+                Console.WriteLine("[--->]");
+                Thread.Sleep(100);
+                Console.Clear();
+                Console.WriteLine("[---->]");
+                Thread.Sleep(100);
+                Console.Clear();
+                Console.WriteLine("[----->]");
+                Thread.Sleep(100);
+                Console.Clear();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+    }
+}
---
+++ PythonUnpacker/PythonUnpacker/unpacker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class Unpacker
+{
+    public static string Unpack(string source)
+    {
+        string code = File.ReadAllText(source);
+        int chars = 0;
+        foreach (char c in code)
+        {
+            chars++;
+        }
+        for (int i=0; i < chars; i++)
+        {
+            code = code.Replace(@"\n", Environment.NewLine);
+            code = code.Replace(@"\r\", Environment.NewLine);
+        }
+        return code;
+    }
+
+
+
+}
+
